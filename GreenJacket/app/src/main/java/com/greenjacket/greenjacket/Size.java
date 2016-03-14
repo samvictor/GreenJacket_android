2091161e2ed2d0fbb79b7f2826fc766703f9bcb6
Sam Inniss <sjinniss1@hotmail.com>
2016-03-14 19:57:50 -0400

    adding size screen

--- GreenJacket/app/src/main/java/com/greenjacket/greenjacket/Size.java
+++ GreenJacket/app/src/main/java/com/greenjacket/greenjacket/Size.java
@@ -9,47 +9,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Container extends AppCompatActivity {
+public class Size extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_container);
+        setContentView(R.layout.activity_size);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Container");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        final Context container_context = this;
+        final Context size_context = this;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Snackbar.make(view, "Are you ready to check out?", Snackbar.LENGTH_LONG)
-                //       .setAction("Action", null).show();
-                Intent to_checkout = new Intent(container_context, Checkout.class);
+                Intent to_checkout = new Intent(size_context, Checkout.class);
                 startActivity(to_checkout);
-
             }
         });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Intent intent = getIntent();
-        //System.out.println("from intent " + intent.getStringExtra("category"));
     }
 
-    public void ContainerBurgerButton(View view) {
+    public void SizeRegularButton(View view) {
         // Do something in response to button
-        Intent to_size = new Intent(this, Size.class);
+        Intent to_options = new Intent(this, Options.class);
 
         switch(view.getId())
         {
             case -1: // burger
-                to_size.putExtra("container", "burger");
+                to_options.putExtra("size", "regular");
                 break;
         }
 
-        to_size.putExtra("item_added", true);
-        startActivity(to_size);
+        to_options.putExtra("item_added", true);
+        startActivity(to_options);
     }
 }
